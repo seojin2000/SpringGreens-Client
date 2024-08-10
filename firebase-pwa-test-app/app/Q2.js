a8d59e62223069481af64e8229978834aab864ad
ITSTIME <88642524+ITSTIME1@users.noreply.github.com>
2024-08-10 14:58:59 +0900

    test : login 11

--- firebase-pwa-test-app/app/Q2.js
+++ firebase-pwa-test-app/app/Q2.js
@@ -5,14 +5,14 @@ import { styled } from '@mui/material/styles';
 const Q1 = styled("div")({
   backgroundColor: `rgba(255, 255, 255, 1)`,
   display: `flex`,
-  flexDirection: `column`, // 변경: 수직 정렬을 위한 flexDirection
+  flexDirection: `column`, // 수직 정렬을 위한 flexDirection
   width: `100vw`,
   height: `100dvh`, // 전체 화면 높이
   justifyContent: `center`, // 중앙 정렬
   alignItems: `center`, // 중앙 정렬
   padding: `0px`,
   boxSizing: `border-box`,
-  position: `fixed`,
+  position: `relative`, // 내부 요소들이 이 요소를 기준으로 위치를 잡음
   overflow: `hidden` // 화면 넘침 방지
 });
 
