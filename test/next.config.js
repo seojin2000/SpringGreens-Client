4bab9e9a92b975efac11f64f2a3e8caff6f9a1b4
ITSTIME <88642524+ITSTIME1@users.noreply.github.com>
2024-08-12 00:15:46 +0900

    test : pwa test

--- test/next.config.js
+++ test/next.config.js
@@ -1,10 +1,21 @@
-// next.config.js
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-});
+const withPWA = require('next-pwa');
 
 module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
+  images: {
+    domains: ['example.com'], 
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://ec2-3-37-50-217.ap-northeast-2.compute.amazonaws.com:9090/api/:path*', 
+      },
+    ];
+  },
 });
--- test/pages/index.js
+++ test/pages/index.js
@@ -33,7 +33,6 @@ export default function Home() {
         <link href="/splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
         <link href="/splashscreens/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
         <link href="/splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-        <link rel='manifest' href='/manifest.json' />
         <link rel="icon" href="/images/icons/icon-192-maskable.png" />
         <Script
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=50912e872dcc098ce3db6b205dd83c96&autoload=false`}
