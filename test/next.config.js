7fbbca4cf740fc49342c90de5650eca6456560bf
ITSTIME <88642524+ITSTIME1@users.noreply.github.com>
2024-08-12 12:58:46 +0900

    fix : proxy

--- test/next.config.js
+++ test/next.config.js
@@ -8,6 +8,14 @@ const withPWA = require('next-pwa')({
 });
 
 const nextConfig = withPWA({
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://ec2-3-37-50-217.ap-northeast-2.compute.amazonaws.com/api/:path*', // 실제 API 서버 URL
+      },
+    ];
+  },
 });
 
 module.exports = nextConfig;
