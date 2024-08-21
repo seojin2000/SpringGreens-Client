a7d3cb281b8ce745b92245d58915be5aedabc6fd
seojin2000 <90614782+seojin2000@users.noreply.github.com>
2024-08-21 22:42:17 +0900

    Update index.js

--- test/pages/mainPage/index.js
+++ test/pages/mainPage/index.js
@@ -8,7 +8,7 @@ const MainPage = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [items, setItems] = useState(generateItems());
   const [prevItems, setPrevItems] = useState([]);
-
+  
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
@@ -44,40 +44,46 @@ const MainPage = () => {
   }, [items]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.topSection}>
-        <div className={styles.menuBox}>
-          <button onClick={toggleMenu} className={styles.menuButton}>
-            ☰
-          </button>
-          <span className={styles.menuText}>나의 현재 위치</span>
-          <span className={styles.menuText}>APM플레이스</span>
-        </div>
-        
-        <div className={styles.headBox}>
+    <div>
+      <div className={styles.container}>
+        <div className={styles.topSection}>
+          <div className={styles.menuAndLocationWrapper}>
+            <button onClick={toggleMenu} className={styles.menuButton}>
+              ☰
+            </button>
+            <div className={styles.myPlaceContainer}>
+              <span className={styles.myPlace}>나의 현재 위치</span>
+              <span>APM플레이스</span>
+            </div>
+          </div>
           
-          <h3>apm 급상승</h3>
-          <FiveSecondTimer onTimerEnd={handleTimerEnd} />
-          <div className={styles.sampleRe} onClick={regenerateItems}></div>
+          <div className={styles.headBox}>
+            <h3>apm 추천상품</h3>
+            <div className={styles.timerAndRestartWapper}>
+              <FiveSecondTimer onTimerEnd={handleTimerEnd} />
+              {/*<div* className={styles.sampleRe} onClick={regenerateItems}></div*/}
+            </div>
+            
+          </div>
         </div>
-      </div>
 
-      <div className={styles.listSection}>
-        <RepeatedList items={items} prevItems={prevItems} />
-      </div>
-
-      {menuOpen && (
-        <div className={styles.menuContent}>
-          <ul className={styles.menuList}>
-            <li>메뉴 항목 1</li>
-            <li>메뉴 항목 2</li>
-            <li>메뉴 항목 3</li>
-          </ul>
+        <div className={styles.listSection}>
+          <RepeatedList items={items} prevItems={prevItems} />
         </div>
-      )}
+
+        {menuOpen && (
+          <div className={styles.menuContent}>
+            <ul className={styles.menuList}>
+              <li>메뉴 항목 1</li>
+              <li>메뉴 항목 2</li>
+              <li>메뉴 항목 3</li>
+            </ul>
+          </div>
+        )}
+        
+      </div>
       <Navbar />
     </div>
-    
   );
 };
 
